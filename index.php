02ff951b15ea098f1fb7865fe39f4ea45560f690
emily <e1640942@cmaisonneuve.qc.ca>
2024-03-04 12:33:21 -0500

    menu entete

--- index.php
+++ index.php
@@ -10,11 +10,19 @@
 </head>
 
 <body>
+  <div id="menu" class="global">
+    <div class="entete__menu">
+      <?php wp_nav_menu(); ?>
+    </div>
+  </div>
+
   <div id="entete" class="global">
     <header class="entete__header">
-      <h1>Thème Emily Joly</h1>
-      <h2>4W4 - Conception d'interface <br>et développement web</h2>
-      <h3>TIM - Collège de Maisonneuve</h3>
+      <div class="entete__header__texte">
+        <h1 class="bgc-text">Thème Emily Joly</h1>
+        <h2 class="bgc-text">4W4 - Conception d'interface <br>et développement web</h2>
+        <h3 class="bgc-text">TIM - Collège de Maisonneuve</h3>
+      </div>
       <button class="entete__button">Évènement</button>
     </header>
     <div class="vague">
@@ -48,15 +56,17 @@
           while (have_posts()) : the_post();
             $titre = get_the_title();
             $sigle = substr($titre, 0, 7);
-            // strpos() = string position
-            $duree = substr($titre, -6);
+            $pos_parenthese = strpos($titre, '(');
+            $duree = substr($titre, $pos_parenthese + 1, -1);
+            // $titre = substr($titre, 7, $pos_parenthese-7);
+            // $duree = substr($titre, -6);
             $nomProg = substr($titre, 7, -6);
         ?>
             <div class="carte">
               <h5> <?php echo $sigle; ?> </h5>
               <!-- <h4> <?php echo $titre; ?> </h4> -->
               <h4> <?php echo $nomProg ?></h4>
-              <h5> <?php echo $duree; ?> </h5>
+              <h5>Durée: <?php echo $duree; ?> </h5>
               <p> <?php echo wp_trim_words(get_the_content(), 10); ?> </p>
             </div>
           <?php endwhile; ?>
